643d571d6d71b609e4b0a00b5efe44656410bef4
Joseph Jegede <ohiwemeh@gmail.com>
2025-06-29 11:47:58 +0100

    first commit

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import CallToActionFooter from '../components/CallToActionFooter'
+
+const Landing = () => {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <Features/>
+      <CallToActionFooter/>
+    </div>
+  )
+}
+
+export default Landing
