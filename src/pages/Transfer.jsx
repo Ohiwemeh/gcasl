51449fb7985f068de3ffaf1bb4ffa3f3bc640c15
Abrahammilli <abrahamoludare17@gmail.com>
2025-07-01 14:35:31 +0100

    first push

---
+++ src/pages/Transfer.jsx
@@ -0,0 +1,41 @@
+// import React from 'react'
+
+// const Transfer = () => {
+//     return (
+//         <div>
+//             <nav> 
+//                 <div>
+//                     Send Money
+//                 </div>
+//                 <div>
+                    
+//                 </div>
+//             </nav>
+//             <form action="">
+//                 <label htmlFor="">
+//                     <p>from</p>
+//                     <input type="text" placeholder='Transfer to' />
+//                 </label>
+
+//                 <label htmlFor="">
+//                     <p>To (Account Number)</p>
+//                     <input type="text" placeholder='Enter recipent account number' />
+//                 </label>
+//                 <label htmlFor="">
+//                     <p>Amount</p>
+//                     <input type="text" placeholder='$0.00' />
+//                 </label>
+//                 <label htmlFor="">
+//                     <p>Note(Optional)</p>
+//                     <input type="text" placeholder='What is this for' />
+//                 </label>
+
+//                 <button className="bg-blue-600 hover:bg-blue-700 transition text-white px-6 py-2 rounded font-semibold">
+//                     send money
+//                 </button>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default Transfer
