37acca82a27ebdc219b773c7276c65ad418797a9
Joseph Jegede <ohiwemeh@gmail.com>
2025-07-10 23:09:46 +0100

    blad

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 // ✅ Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://gcasl.vercel.app",
   credentials: true,
 }));
 app.use(express.json({ limit: "10mb" }));
